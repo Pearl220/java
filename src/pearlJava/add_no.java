be16dfe6c94e7fd819ae1054101e4fc59bece6f4
Pearl Fernandes <pearlfeanands0631@gmail.com>
2024-01-29 22:35:03 +0530

    commit

--- src/pearlJava/add_no.java
+++ src/pearlJava/add_no.java
@@ -1,10 +1,24 @@
 package pearlJava;
-
+import java.util.Scanner;
 public class add_no {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a,b;
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Enter the first number: ");
+		a = scan.nextInt();
+		System.out.println("Enter the seconf number: ");
+		b = scan.nextInt();
+		
+		int result = add(a,b);
+		System.out.println("Sum: " + result);
 	}
+	public static int add(int a,int b) {
+		int sum = a+b;
+		return sum;
+	}
+
 
 }
